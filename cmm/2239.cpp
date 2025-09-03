be5fc2767b6c7a15579ea36d62e096bb58a432bf
Hyehyeon.Kim <dev.hyehyeon.kim@gmail.com>
2025-09-04 07:16:48 +0900

    2239

---
+++ cmm/2239.cpp
@@ -0,0 +1,168 @@
+//#pragma disable(4996)
+//
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+//typedef signed int int32;
+//typedef unsigned int uint32;
+//typedef long long int64;
+//typedef unsigned long long uint64;
+//
+//int32 map[9][9];
+//int32 solve[9][9];
+//
+//struct Point {
+//	int32 r;
+//	int32 c;
+//	int32* invalids;
+//	int32 invalidCount;
+//};
+//
+//Point zeros[81];
+//
+//class MyQueue {
+//private:
+//	int32 maxN;
+//	Point* values;
+//	int32 count;
+//	int32 s, e;
+//public:
+//	MyQueue(int32 maxN) {
+//		this->maxN = maxN;
+//		this->values = new Point[maxN];
+//		this->count = 0;
+//		this->s = 0;
+//		this->e = 0;
+//	}
+//public:
+//	void Push(Point value) {
+//		values[e++] = value;
+//		count++;
+//	}
+//
+//	Point Pop() {
+//		Point ret = values[s];
+//		s++;
+//		count--;
+//		return ret;
+//	}
+//
+//	Point Front() {
+//		return values[s];
+//	}
+//
+//	bool IsEmpty() {
+//		return count == 0;
+//	}
+//
+//	void Recover() {
+//		if (s == 0) {
+//			throw new exception();
+//		}
+//
+//		s--;
+//		count++;
+//	}
+//};
+//
+//bool isValid(Point p, int32 value) {
+//	for (int i = 0; i < 9; i++) {
+//		if (map[i][p.c] == value) return false;
+//		if (map[p.r][i] == value) return false;
+//	}
+//
+//	int r = p.r - p.r % 3;
+//	int c = p.c - p.c % 3;
+//
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			if (map[r + i][c + j] == value) return false;
+//		}
+//	}
+//
+//	return true;
+//}
+//
+//bool hasValid(Point p) {
+//	int32 start = map[p.r][p.c];
+//
+//	for (int i = start + 1; i <= 9; i++) {
+//		if (isValid(p, i)) return true;
+//	}
+//
+//	return false;
+//}
+//
+//void printMap() {
+//	for (int i = 0; i < 9; i++) {
+//		for (int j = 0; j < 9; j++) {
+//			cout << map[i][j];
+//		}
+//
+//		cout << "\n";
+//	}
+//}
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	MyQueue* points = new MyQueue(81);
+//
+//	for (int32 i = 0; i < 9; i++) {
+//		string cur;
+//		cin >> cur;
+//
+//		for (int j = 0; j < 9; j++) {
+//			map[i][j] = cur[j] - '0';
+//			solve[i][j] = cur[j] - '0';
+//
+//			if (map[i][j] == 0) {
+//				Point zero;
+//				zero.r = i;
+//				zero.c = j;
+//				zero.invalids = new int32[9];
+//				zero.invalidCount = 0;
+//				points->Push(zero);
+//			}
+//		}
+//	}
+//
+//	while (!points->IsEmpty()) {
+//		Point cur = points->Front();
+//
+//		int32 startPoint = map[cur.r][cur.c] + 1;
+//		map[cur.r][cur.c] = 0;
+//
+//		bool isSolved = false;
+//
+//		for (int i = startPoint; i <= 9; i++) {
+//			if (isValid(cur, i)) {
+//				map[cur.r][cur.c] = i;
+//				isSolved = true;
+//				break;
+//			}
+//		}
+//
+//		if (map[cur.r][cur.c] == 0) {
+//			points->Recover();
+//			Point prev = points->Front();
+//
+//			while (!hasValid(prev)) {
+//				map[prev.r][prev.c] = 0;
+//				points->Recover();
+//				prev = points->Front();
+//			}
+//		}
+//		else {
+//			points->Pop();
+//		}
+//	}
+//
+//	printMap();
+//
+//	return 0;
+//}
--- cmm/main.cpp
+++ cmm/main.cpp
@@ -10,38 +10,98 @@ typedef unsigned int uint32;
 typedef long long int64;
 typedef unsigned long long uint64;
 
-const int32 MAX_N = 8;
+int32 map[9][9];
+int32 solve[9][9];
 
-int32 Arr[MAX_N];
-int32 CopiedArr[MAX_N];
+struct Point {
+	int32 r;
+	int32 c;
+	int32* invalids;
+	int32 invalidCount;
+};
 
-void mergeSort(int32 s, int32 e, int32* arr, int32* copiedArr) {
-	if (e - s == 1) return;
+Point zeros[81];
 
-	int32 mid = s + (e - s) / 2.0;
-	mergeSort(s, mid, arr, copiedArr);
-	mergeSort(mid, e, arr, copiedArr);
-
-	for (int32 i = s; i < e; i++) {
-		copiedArr[i] = arr[i];
+class MyQueue {
+private:
+	int32 maxN;
+	Point* values;
+	int32 count;
+	int32 s, e;
+public:
+	MyQueue(int32 maxN) {
+		this->maxN = maxN;
+		this->values = new Point[maxN];
+		this->count = 0;
+		this->s = 0;
+		this->e = 0;
+	}
+public:
+	void Push(Point value) {
+		values[e++] = value;
+		count++;
 	}
 
-	int32 left = s;
-	int32 right = mid;
+	Point Pop() {
+		Point ret = values[s];
+		s++;
+		count--;
+		return ret;
+	}
 
-	for (int32 i = s; i < e; i++) {
-		if (right == e) {
-			arr[i] = copiedArr[left++];
+	Point Front() {
+		return values[s];
+	}
+
+	bool IsEmpty() {
+		return count == 0;
+	}
+
+	void Recover() {
+		if (s == 0) {
+			throw new exception();
 		}
-		else if (left == mid) {
-			arr[i] = copiedArr[right++];
+
+		s--;
+		count++;
+	}
+};
+
+bool isValid(Point p, int32 value) {
+	for (int i = 0; i < 9; i++) {
+		if (map[i][p.c] == value) return false;
+		if (map[p.r][i] == value) return false;
+	}
+
+	int r = p.r - p.r % 3;
+	int c = p.c - p.c % 3;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (map[r + i][c + j] == value) return false;
 		}
-		else if (copiedArr[left] < copiedArr[right]) {
-			arr[i] = copiedArr[left++];
+	}
+
+	return true;
+}
+
+bool hasValid(Point p) {
+	int32 start = map[p.r][p.c];
+
+	for (int i = start + 1; i <= 9; i++) {
+		if (isValid(p, i)) return true;
+	}
+
+	return false;
+}
+
+void printMap() {
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			cout << map[i][j];
 		}
-		else {
-			arr[i] = copiedArr[right++];
-		}
+
+		cout << "\n";
 	}
 }
 
@@ -50,51 +110,59 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int32 N, M;
-	cin >> N >> M;
+	MyQueue* points = new MyQueue(81);
 
-	for (int i = 0; i < N; i++) {
-		cin >> Arr[i];
-	}
+	for (int32 i = 0; i < 9; i++) {
+		string cur;
+		cin >> cur;
 
-	mergeSort(0, N, Arr, CopiedArr);
+		for (int j = 0; j < 9; j++) {
+			map[i][j] = cur[j] - '0';
+			solve[i][j] = cur[j] - '0';
 
-	int32* indexArr = new int32[M];
-
-	for (int i = 0; i < M; i++) {
-		indexArr[i] = i;
-	}
-
-	bool isDone = false;
-
-	while (!isDone) {
-		for (int i = 0; i < M; i++) {
-			cout << Arr[indexArr[i]] << " ";
+			if (map[i][j] == 0) {
+				Point zero;
+				zero.r = i;
+				zero.c = j;
+				zero.invalids = new int32[9];
+				zero.invalidCount = 0;
+				points->Push(zero);
+			}
 		}
-		cout << "\n";
-
-		int32 m = M - 1;
-
-		while (true) {
-			if (m < 0) {
-				isDone = true;
-				break;
-			}
-
-			indexArr[m]++;
-
-			if (indexArr[m] == (N + m - (M - 1))) {
-				m--;
-				continue;
-			}
-			else {
-				for (int32 i = m + 1; i < M; i++) {
-					indexArr[i] = indexArr[i - 1] + 1;
-				}
-				break;
-			}
-		}		
 	}
+
+	while (!points->IsEmpty()) {
+		Point cur = points->Front();
+
+		int32 startPoint = map[cur.r][cur.c] + 1;
+		map[cur.r][cur.c] = 0;
+
+		bool isSolved = false;
+
+		for (int i = startPoint; i <= 9; i++) {
+			if (isValid(cur, i)) {
+				map[cur.r][cur.c] = i;
+				isSolved = true;
+				break;
+			}
+		}
+
+		if (map[cur.r][cur.c] == 0) {
+			points->Recover();
+			Point prev = points->Front();
+
+			while (!hasValid(prev)) {
+				map[prev.r][prev.c] = 0;
+				points->Recover();
+				prev = points->Front();
+			}
+		}
+		else {
+			points->Pop();
+		}
+	}
+
+	printMap();	
 
 	return 0;
 }
