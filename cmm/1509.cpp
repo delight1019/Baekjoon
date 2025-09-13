91ded945949840ad4931cc6776f07bcc679a69cd
Hyehyeon.Kim <dev.hyehyeon.kim@gmail.com>
2025-09-13 10:42:47 +0900

    1509

---
+++ cmm/1509.cpp
@@ -0,0 +1,64 @@
+//#pragma disable(4996)
+//
+//#include <iostream>
+//#include <cstring>
+//
+//using namespace std;
+//
+//typedef signed short int16;
+//typedef unsigned short uint16;
+//typedef signed int int32;
+//typedef unsigned int uint32;
+//typedef long long int64;
+//typedef unsigned long long uint64;
+//typedef unsigned char ubyte;
+//
+//const int32 MAX_VALUE = 10000;
+//
+//string inputStr;
+//int32 cache[2500];
+//
+//int32 getMin(int32 a, int32 b) {
+//	return a < b ? a : b;
+//}
+//
+//bool isPelindrome(int32 s, int32 e) {
+//	while (s <= e) {
+//		if (inputStr[s] != inputStr[e]) return false;
+//
+//		s++;
+//		e--;
+//	}
+//
+//	return true;
+//}
+//
+//int32 solve(int32 cur) {
+//	if (cur == inputStr.length()) return 0;
+//
+//	int32& ret = cache[cur];
+//	if (ret != -1) return ret;
+//
+//	ret = MAX_VALUE;
+//
+//	for (int32 i = cur + 1; i <= inputStr.length(); i++) {
+//		if (isPelindrome(cur, i - 1)) {
+//			ret = getMin(ret, 1 + solve(i));
+//		}
+//	}
+//
+//	return ret;
+//}
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	cin >> inputStr;
+//
+//	memset(cache, -1, sizeof(cache));
+//	cout << solve(0);
+//
+//	return 0;
+//}
--- cmm/main.cpp
+++ cmm/main.cpp
@@ -13,35 +13,41 @@ typedef long long int64;
 typedef unsigned long long uint64;
 typedef unsigned char ubyte;
 
-const int32 MAX_N = 20;
-const int32 MAX_M = 10000;
-int32 coins[MAX_N];
+const int32 MAX_VALUE = 10000;
 
-int32 cache[MAX_M + 1][MAX_N];
+string inputStr;
+int32 cache[2500];
 
-int32 getMax(int32 a, int32 b) {
-	return a > b ? a : b;
+int32 getMin(int32 a, int32 b) {
+	return a < b ? a : b;
 }
 
-int32 solve(int32 cur, int32 N, int32 m) {
-	if (m == 0) return 1;
-	if (cur == N) return 0;
+bool isPelindrome(int32 s, int32 e) {
+	while (s <= e) {
+		if (inputStr[s] != inputStr[e]) return false;
 
-	int32& ret = cache[m][cur];
-
-	if (ret != -1) return ret;
-
-	int32 retValue = 0;
-
-	int32 curCoin = coins[cur];
-	int32 temp = m;
-
-	while (temp >= 0) {
-		retValue += solve(cur + 1, N, temp);
-		temp -= curCoin;
+		s++;
+		e--;
 	}
 
-	return (ret = retValue);
+	return true;
+}
+
+int32 solve(int32 cur) {
+	if (cur == inputStr.length()) return 0;
+
+	int32& ret = cache[cur];
+	if (ret != -1) return ret;
+
+	ret = MAX_VALUE;
+
+	for (int32 i = cur + 1; i <= inputStr.length(); i++) {
+		if (isPelindrome(cur, i - 1)) {
+			ret = getMin(ret, 1 + solve(i));
+		}
+	}
+
+	return ret;
 }
 
 int main() {
@@ -49,23 +55,10 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int32 T;
-	cin >> T;
+	cin >> inputStr;
 
-	for (int32 testCase = 0; testCase < T; testCase++) {
-		int32 N, M;
-		cin >> N;
-
-		for (int32 i = 0; i < N; i++) {
-			cin >> coins[i];
-		}
-
-		cin >> M;
-
-		memset(cache, -1, sizeof(cache));
-		int32 ret = solve(0, N, M);
-		cout << ret << "\n";
-	}
+	memset(cache, -1, sizeof(cache));
+	cout << solve(0);
 
 	return 0;
 }                                      
