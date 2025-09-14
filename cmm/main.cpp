6d6d58c20d73e982e1861cee7e377a2f3be9b91e
Hyehyeon.Kim <dev.hyehyeon.kim@gmail.com>
2025-09-14 09:12:13 +0900

    28707

--- cmm/main.cpp
+++ cmm/main.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <cstring>
+#include <queue>
+#include <map>
 
 using namespace std;
 
@@ -13,38 +15,151 @@ typedef long long int64;
 typedef unsigned long long uint64;
 typedef unsigned char ubyte;
 
-const int32 MAX_VALUE = 10000;
+const int32 MAX_VALUE = 1000000;
 
-string inputStr;
-int32 cache[2500];
+struct MyOper {
+	int32 l;
+	int32 r;
+	int32 c;
+};
+
+int32 AArr[8];
+int32 retArr[11];
+MyOper Opers[10];
+map<int32, int32> cacheMap;
+
+struct Node {
+	int32 num;
+	int32 cost;
+
+	bool operator < (const Node& other) const {
+		return cost < other.cost;
+	}
+
+	bool operator > (const Node& other) const {
+		return cost > other.cost;
+	}
+};
+
+int32* numtoArr(int32 num) {
+	int32* ret = new int32[8];
+	int32 curIndex = 7;
+
+	while (num > 0) {
+		int32 temp = num % 10;
+
+		if (temp > 0) {
+			ret[curIndex--] = temp;
+			num /= 10;
+		}
+		else {
+			temp = num % 100;
+			ret[curIndex--] = temp;
+			num /= 100;
+		}
+	}
+
+	if (curIndex >= 0) {
+		for (int i = curIndex + 1; i < 8; i++) {
+			ret[i - (curIndex + 1)] = ret[i];
+		}
+	}
+
+	return ret;
+}
+
+int32 getNumber(int32* arr, int32 N) {
+	int32 ret = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (arr[i] == 10) {
+			ret *= 100;
+			ret += arr[i];
+		}
+		else {
+			ret *= 10;
+			ret += arr[i];
+		}
+	}
+
+	return ret;
+}
 
 int32 getMin(int32 a, int32 b) {
 	return a < b ? a : b;
 }
 
-bool isPelindrome(int32 s, int32 e) {
-	while (s <= e) {
-		if (inputStr[s] != inputStr[e]) return false;
-
-		s++;
-		e--;
-	}
-
-	return true;
+void doOper(int32* arr, MyOper oper) {
+	int32 temp = arr[oper.l - 1];
+	arr[oper.l - 1] = arr[oper.r - 1];
+	arr[oper.r - 1] = temp;
 }
 
-int32 solve(int32 cur) {
-	if (cur == inputStr.length()) return 0;
+int32 doOper(int32 num, MyOper oper, int32 N) {
+	int32* arr = numtoArr(num);
+	doOper(arr, oper);
+	int32 ret = getNumber(arr, N);
+	delete[] arr;
+	return ret;
+}
 
-	int32& ret = cache[cur];
-	if (ret != -1) return ret;
+void solve(int32 startNum, int32 N, int32 M) {
+	priority_queue<Node, vector<Node>, greater<Node>> pq;	
+	Node start;
+	start.num = startNum;
+	start.cost = 0;
+	pq.push(start);
+	cacheMap.insert(make_pair(start.num, 0));
 
-	ret = MAX_VALUE;
+	while (!pq.empty()) {
+		Node cur = pq.top();
+		pq.pop();
 
-	for (int32 i = cur + 1; i <= inputStr.length(); i++) {
-		if (isPelindrome(cur, i - 1)) {
-			ret = getMin(ret, 1 + solve(i));
+		if (cacheMap.find(cur.num) != cacheMap.end()) {
+			if (cacheMap[cur.num] < cur.cost) continue;
 		}
+
+		for (int i = 0; i < M; i++) {
+			MyOper oper = Opers[i];
+			int32 next = doOper(cur.num, oper, N);
+
+			if (cacheMap.find(next) == cacheMap.end()) {
+				cacheMap.insert(make_pair(next, cur.cost + oper.c));
+				Node nextNode;
+				nextNode.num = next;
+				nextNode.cost = cur.cost + oper.c;
+				pq.push(nextNode);
+			}
+			else if (cacheMap[next] > cur.cost + oper.c) {
+				cacheMap[next] = cur.cost + oper.c;
+				Node nextNode;
+				nextNode.num = next;
+				nextNode.cost = cur.cost + oper.c;
+				pq.push(nextNode);
+			}
+		}
+	}
+}
+
+int32 findSol(int32 N) {
+	memset(retArr, 0, sizeof(retArr));
+
+	for (int i = 0; i < N; i++) {
+		retArr[AArr[i]]++;
+	}
+
+	int32 ret = 0;
+
+	for (int i = 1; i < 10; i++) {
+		for (int j = 0; j < retArr[i]; j++) {
+			ret *= 10;
+			ret += i;
+		}
+	}
+
+	for (int j = 0; j < retArr[10]; j++) {
+		ret *= 100;
+		ret += 10;
 	}
 
 	return ret;
@@ -55,10 +170,29 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> inputStr;
+	int32 N, M;
+	cin >> N;
 
-	memset(cache, -1, sizeof(cache));
-	cout << solve(0);
+	for (int i = 0; i < N; i++) {
+		cin >> AArr[i];
+	}
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		cin >> Opers[i].l >> Opers[i].r >> Opers[i].c;
+	}
+
+	int32 startNum = getNumber(AArr, N);
+	solve(startNum, N, M);
+	int32 solValue = findSol(N);
+
+	if (cacheMap.find(solValue) == cacheMap.end()) {
+		cout << -1;
+	}
+	else {
+		cout << cacheMap[solValue];
+	}
 
 	return 0;
 }                                      
