180656365c1d3f8284c490742c8513d08ae2e967
Hyehyeon.Kim <dev.hyehyeon.kim@gmail.com>
2025-09-20 16:20:53 +0900

    12850

---
+++ cmm/12850.cpp
@@ -0,0 +1,90 @@
+//#pragma disable(4996)
+//
+//#include <iostream>
+//#include <cstring>
+//#include <vector>
+//#include <queue>
+//
+//using namespace std;
+//
+//typedef signed short int16;
+//typedef unsigned short uint16;
+//typedef signed int int32;
+//typedef unsigned int uint32;
+//typedef long long int64;
+//typedef unsigned long long uint64;
+//typedef unsigned char ubyte;
+//
+//#define MAX(a, b) a < b ? b : a;
+//#define MIN(a, b) a < b ? a : b;
+//
+//const int64 DENO = 1000000007;
+//
+//using MAT = vector<vector<uint64>>;
+//
+//const MAT factor = {
+//	{0, 1, 1, 0, 0, 0, 0, 0}, // 0
+//	{1, 0, 1, 1, 0, 0, 0, 0}, // 1
+//	{1, 1, 0, 1, 1, 0, 0, 0}, // 2
+//	{0, 1, 1, 0, 1, 1, 0, 0}, // 3
+//	{0, 0, 1, 1, 0, 1, 1, 0}, // 4
+//	{0, 0, 0, 1, 1, 0, 0, 1}, // 5
+//	{0, 0, 0, 0, 1, 0, 0, 1}, // 6
+//	{0, 0, 0, 0, 0, 1, 1, 0}, // 7
+//};
+//
+//const MAT id = {
+//	{1, 0, 0, 0, 0, 0, 0, 0},
+//	{0, 1, 0, 0, 0, 0, 0, 0},
+//	{0, 0, 1, 0, 0, 0, 0, 0},
+//	{0, 0, 0, 1, 0, 0, 0, 0},
+//	{0, 0, 0, 0, 1, 0, 0, 0},
+//	{0, 0, 0, 0, 0, 1, 0, 0},
+//	{0, 0, 0, 0, 0, 0, 1, 0},
+//	{0, 0, 0, 0, 0, 0, 0, 1}
+//};
+//
+//MAT multiplyMat(const MAT& mat1, const MAT& mat2) {
+//	MAT ret(8, vector<uint64>(8));
+//
+//	for (int i = 0; i < 8; i++) {
+//		for (int j = 0; j < 8; j++) {
+//			uint64 temp = 0;
+//
+//			for (int k = 0; k < 8; k++) {
+//				temp += mat1[i][k] * mat2[k][j];
+//				temp %= DENO;
+//			}
+//
+//			ret[i][j] = temp;
+//		}
+//	}
+//
+//	return ret;
+//}
+//
+//MAT solve(int32 d) {
+//	if (d == 0) return id;
+//	if (d == 1) return factor;
+//
+//	MAT temp = solve(d / 2);
+//	MAT ret = multiplyMat(temp, temp);
+//
+//	return d % 2 == 0 ? ret : multiplyMat(ret, factor);
+//}
+//
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	int64 D;
+//	cin >> D;
+//
+//	MAT ansMat = solve(D);
+//
+//	cout << ansMat[0][0];
+//
+//	return 0;
+//}
--- cmm/main.cpp
+++ cmm/main.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -16,119 +18,73 @@ typedef unsigned char ubyte;
 #define MAX(a, b) a < b ? b : a;
 #define MIN(a, b) a < b ? a : b;
 
-int32 getMax(int32 a, int32 b) {
-	return MAX(a, b);
-}
+const int64 DENO = 1000000007;
 
-int32 getMin(int32 a, int32 b) {
-	return MIN(a, b);
-}
+using MAT = vector<vector<uint64>>;
 
-struct Point {
-	int32 x;
-	int32 y;
+const MAT factor = {
+	{0, 1, 1, 0, 0, 0, 0, 0}, // 0
+	{1, 0, 1, 1, 0, 0, 0, 0}, // 1
+	{1, 1, 0, 1, 1, 0, 0, 0}, // 2
+	{0, 1, 1, 0, 1, 1, 0, 0}, // 3
+	{0, 0, 1, 1, 0, 1, 1, 0}, // 4
+	{0, 0, 0, 1, 1, 0, 0, 1}, // 5
+	{0, 0, 0, 0, 1, 0, 0, 1}, // 6
+	{0, 0, 0, 0, 0, 1, 1, 0}, // 7
 };
 
-struct Line {
-	Point p1;
-	Point p2;
+const MAT id = {
+	{1, 0, 0, 0, 0, 0, 0, 0},
+	{0, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 1, 0, 0, 0, 0, 0},
+	{0, 0, 0, 1, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0},
+	{0, 0, 0, 0, 0, 1, 0, 0},
+	{0, 0, 0, 0, 0, 0, 1, 0},
+	{0, 0, 0, 0, 0, 0, 0, 1}
 };
 
-const int32 MAX_N = 3000;
+MAT multiplyMat(const MAT& mat1, const MAT& mat2) {
+	MAT ret(8, vector<uint64>(8));
 
-Line lines[MAX_N];
+	for (int i = 0; i < 8; i++) {		
+		for (int j = 0; j < 8; j++) {
+			uint64 temp = 0;
 
-int32 ccw(Point p1, Point p2, Point p3) {
-	int32 temp = (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y);
-	return temp > 0 ?
-		1 :
-		temp < 0 ?
-		-1 : 0;
-}
+			for (int k = 0; k < 8; k++) {
+				temp += mat1[i][k] * mat2[k][j];
+				temp %= DENO;
+			}
 
-bool isIntersect(Line l1, Line l2) {
-	int c1 = ccw(l1.p1, l1.p2, l2.p1) * ccw(l1.p1, l1.p2, l2.p2);
-	int c2 = ccw(l2.p1, l2.p2, l1.p1) * ccw(l2.p1, l2.p2, l1.p2);
-
-	if (c1 <= 0 && c2 <= 0) {
-		if (c1 == 0 && c2 == 0) {
-			return (getMin(l1.p1.x, l1.p2.x) <= getMax(l2.p1.x, l2.p2.x)) &&
-				(getMin(l2.p1.x, l2.p2.x) <= getMax(l1.p1.x, l1.p2.x)) &&
-				(getMin(l1.p1.y, l1.p2.y) <= getMax(l2.p1.y, l2.p2.y)) &&
-				(getMin(l2.p1.y, l2.p2.y) <= getMax(l1.p1.y, l1.p2.y));
-
+			ret[i][j] = temp;
 		}
-
-		return true;
 	}
 
-	return false;
+	return ret;
 }
 
-int32 parents[MAX_N];
-int32 sizes[MAX_N];
-bool checked[MAX_N] = { false, };
+MAT solve(int32 d) {
+	if (d == 0) return id;
+	if (d == 1) return factor;
 
-int32 find(int32 x) {
-	if (parents[x] == x) return x;
+	MAT temp = solve(d / 2);
+	MAT ret = multiplyMat(temp, temp);
 
-	parents[x] = find(parents[x]);
-	return parents[x];
+	return d % 2 == 0 ? ret : multiplyMat(ret, factor);
 }
 
-void doUnion(int32 x, int32 y) {
-	x = find(x);
-	y = find(y);
-
-	if (x == y) return;
-
-	if (sizes[x] > sizes[y]) {
-		parents[y] = x;
-		sizes[x] += sizes[y];
-	}
-	else {
-		parents[x] = y;
-		sizes[y] += sizes[x];
-	}
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int32 N;
-	cin >> N;
+	int64 D;
+	cin >> D;
 
-	for (int32 n = 0; n < N; n++) {
-		Line& curline = lines[n];
-		cin >> curline.p1.x >> curline.p1.y >> curline.p2.x >> curline.p2.y;
+	MAT ansMat = solve(D);
 
-		parents[n] = n;
-		sizes[n] = 1;
-	}
-
-	for (int32 i = 0; i < N; i++) {
-		for (int32 j = i + 1; j < N; j++) {
-			if (isIntersect(lines[i], lines[j])) {
-				doUnion(i, j);
-			}
-		}
-	}
-
-	int32 retCount = 0;
-	int32 retMax = 0;
-
-	for (int32 i = 0; i < N; i++) {
-		if (!checked[find(i)]) {
-			checked[find(i)] = true;
-			retMax = MAX(retMax, sizes[find(i)]);
-			retCount++;
-		}
-	}
-
-	cout << retCount << "\n";
-	cout << retMax;
+	cout << ansMat[0][0];
 
 	return 0;
 }                                      
