e534da079ec73b8e2b8838fd30b86a6a1e6c9bb4
Hyehyeon.Kim <dev.hyehyeon.kim@gmail.com>
2025-11-09 22:45:11 +0900

    31668

--- cmm/main.cpp
+++ cmm/main.cpp
@@ -21,39 +21,15 @@ typedef unsigned char ubyte;
 #define MIN(a, b) a < b ? a : b
 #define ABS(a) a < 0 ? -a : a
 
-bool InRange(int32 v, int32 m, int32 M) {
-	return m <= v && v < M;
-}
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int waveLength;
-	cin >> waveLength;
+	int32 N, M, K;
+	cin >> N >> M >> K;
 
-	if (InRange(waveLength, 620, 780) || waveLength == 780) {
-		cout << "Red";
-	}
-	else if (InRange(waveLength, 590, 620)) {
-		cout << "Orange";
-	}
-	else if (InRange(waveLength, 570, 590)) {
-		cout << "Yellow";
-	}
-	else if (InRange(waveLength, 495, 570)) {
-		cout << "Green";
-	}
-	else if (InRange(waveLength, 450, 495)) {
-		cout << "Blue";
-	}
-	else if (InRange(waveLength, 425, 450)) {
-		cout << "Indigo";
-	}
-	else if (InRange(waveLength, 380, 425)) {
-		cout << "Violet";
-	}
+	cout << (M / N) * K;
 
 	return 0;
 }
