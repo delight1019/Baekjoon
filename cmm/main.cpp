33ba3b313bec48a784988acda85551ecaf6b2a9e
Hyehyeon.Kim <dev.hyehyeon.kim@gmail.com>
2025-11-13 21:12:30 +0900

    3085

--- cmm/main.cpp
+++ cmm/main.cpp
@@ -21,6 +21,11 @@ typedef unsigned char ubyte;
 #define MIN(a, b) a < b ? a : b
 #define ABS(a) a < 0 ? -a : a
 
+const int DELTA_R[4] = { 1, -1, 0, 0 };
+const int DELTA_C[4] = { 0, 0, 1, -1 };
+
+char candies[50][50];
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -29,20 +34,68 @@ int main() {
 	int32 N;
 	cin >> N;
 
-	for (int32 n = 0; n < N; n++) {
-		int32 M = N + n;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> candies[i][j];
+		}
+	}
 
-		for (int32 m = 1; m <= M; m++) {
-			if (m == (N - n) || m == (N + n)) {
-				cout << '*';
-			}
-			else {
-				cout << ' ';
+	int32 ret = 0;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < 4; k++) {
+				int r = i + DELTA_R[k];
+				int c = j + DELTA_C[k];
+
+				if (r >= 0 && r < N && c >= 0 && c < N) {
+					char prev = candies[i][j];
+					char cur = candies[r][c];
+
+					candies[i][j] = cur;
+					candies[r][c] = prev;
+
+					int row = 1;
+					int col = 1;
+
+					for (int tr = i - 1; tr >= 0; tr--) {
+						if (candies[tr][j] == cur) {
+							row++;
+						}
+						else break;
+					}
+
+					for (int tr = i + 1; tr < N; tr++) {
+						if (candies[tr][j] == cur) {
+							row++;
+						}
+						else break;
+					}
+
+					for (int tc = j - 1; tc >= 0; tc--) {
+						if (candies[i][tc] == cur) {
+							col++;
+						}
+						else break;
+					}
+					for (int tc = j + 1; tc < N; tc++) {
+						if (candies[i][tc] == cur) {
+							col++;
+						}
+						else break;
+					}
+
+					candies[i][j] = prev;
+					candies[r][c] = cur;
+
+					ret = MAX(ret, row);
+					ret = MAX(ret, col);
+				}
 			}
 		}
-
-		cout << "\n";
 	}
+
+	cout << ret;
 
 	return 0;
 }
