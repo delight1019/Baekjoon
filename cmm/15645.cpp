8cc1ef6d2c58bfe8070f47f430e0dd75c52771f5
Hyehyeon.Kim <dev.hyehyeon.kim@gmail.com>
2025-12-01 21:09:27 +0900

    15645

---
+++ cmm/15645.cpp
@@ -0,0 +1,82 @@
+//#pragma disable(4996)
+//
+//#include <iostream>
+//#include <cstring>
+//#include <cmath>
+//
+//using namespace std;
+//
+//typedef signed short int16;
+//typedef unsigned short uint16;
+//typedef signed int int32;
+//typedef unsigned int uint32;
+//typedef long long int64;
+//typedef unsigned long long uint64;
+//typedef unsigned char ubyte;
+//
+//#define MAX(a, b) a < b ? b : a
+//#define MIN(a, b) a < b ? a : b
+//#define ABS(a) a < 0 ? -a : a
+//
+//const int32 MAX_N = 100000;
+//
+//int32 N;
+//
+//int32 board[MAX_N + 1][3];
+//int32 maxCache[MAX_N + 1][3];
+//int32 minCache[MAX_N + 1][3];
+//
+//int32 calculateMax(int32 r, int32 c) {
+//	if (r > N || c < 0 || c >= 3) return 0;
+//
+//	int32& ret = maxCache[r][c];
+//
+//	if (ret != -1) return ret;
+//
+//	ret = calculateMax(r + 1, c - 1);
+//	ret = MAX(ret, calculateMax(r + 1, c));
+//	ret = MAX(ret, calculateMax(r + 1, c + 1));
+//
+//	ret += board[r][c];
+//
+//	return ret;
+//}
+//
+//int32 calculateMin(int32 r, int32 c) {
+//	if (r > N || c < 0 || c >= 3) return 10000000;
+//	if (r == N) return board[r][c];
+//
+//	int32& ret = minCache[r][c];
+//
+//	if (ret != -1) return ret;
+//
+//	ret = calculateMin(r + 1, c - 1);
+//	ret = MIN(ret, calculateMin(r + 1, c));
+//	ret = MIN(ret, calculateMin(r + 1, c + 1));
+//
+//	ret += board[r][c];
+//
+//	return ret;
+//}
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	memset(board, 0, sizeof(board));
+//	memset(maxCache, -1, sizeof(maxCache));
+//	memset(minCache, -1, sizeof(minCache));
+//
+//	cin >> N;
+//
+//	for (int32 n = 1; n <= N; n++) {
+//		for (int32 c = 0; c < 3; c++) {
+//			cin >> board[n][c];
+//		}
+//	}
+//
+//	cout << calculateMax(0, 1) << " " << calculateMin(0, 1);
+//
+//	return 0;
+//}
--- cmm/main.cpp
+++ cmm/main.cpp
@@ -1,6 +1,7 @@
 #pragma disable(4996)
 
 #include <iostream>
+#include <cstring>
 #include <cmath>
 
 using namespace std;
@@ -17,31 +18,65 @@ typedef unsigned char ubyte;
 #define MIN(a, b) a < b ? a : b
 #define ABS(a) a < 0 ? -a : a
 
+const int32 MAX_N = 100000;
+
+int32 N;
+
+int32 board[MAX_N + 1][3];
+int32 maxCache[MAX_N + 1][3];
+int32 minCache[MAX_N + 1][3];
+
+int32 calculateMax(int32 r, int32 c) {
+	if (r > N || c < 0 || c >= 3) return 0;
+
+	int32& ret = maxCache[r][c];
+
+	if (ret != -1) return ret;
+
+	ret = calculateMax(r + 1, c - 1);
+	ret = MAX(ret, calculateMax(r + 1, c));
+	ret = MAX(ret, calculateMax(r + 1, c + 1));
+
+	ret += board[r][c];
+
+	return ret;
+}
+
+int32 calculateMin(int32 r, int32 c) {
+	if (r > N || c < 0 || c >= 3) return 10000000;
+	if (r == N) return board[r][c];
+
+	int32& ret = minCache[r][c];
+
+	if (ret != -1) return ret;
+
+	ret = calculateMin(r + 1, c - 1);
+	ret = MIN(ret, calculateMin(r + 1, c));
+	ret = MIN(ret, calculateMin(r + 1, c + 1));
+
+	ret += board[r][c];
+
+	return ret;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int32 T;
-	cin >> T;
+	memset(board, 0, sizeof(board));
+	memset(maxCache, -1, sizeof(maxCache));
+	memset(minCache, -1, sizeof(minCache));
 
-	for (int32 testCase = 1; testCase <= T; testCase++) {
-		uint64 n, m;
-		cin >> n >> m;
+	cin >> N;
 
-		if (n == 1 || m == 1) {
-			cout << "YES" << "\n";
-			continue;
-		}
-
-		if ((n + m - 4) % 2 == 0) {
-			cout << "NO" << "\n";
-		}
-		else {
-			cout << "YES" << "\n";
+	for (int32 n = 1; n <= N; n++) {
+		for (int32 c = 0; c < 3; c++) {
+			cin >> board[n][c];
 		}
 	}
+
+	cout << calculateMax(0, 1) << " " << calculateMin(0, 1);
 
 	return 0;
 }
