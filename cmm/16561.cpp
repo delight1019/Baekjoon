f10ab6a410fd76efddf8b56c85e7fa2efb41ecb3
Hyehyeon.Kim <dev.hyehyeon.kim@gmail.com>
2025-12-03 21:33:24 +0900

    16561

---
+++ cmm/16561.cpp
@@ -0,0 +1,44 @@
+//#pragma disable(4996)
+//
+//#include <iostream>
+//#include <cstring>
+//#include <cmath>
+//
+//using namespace std;
+//
+//typedef signed short int16;
+//typedef unsigned short uint16;
+//typedef signed int int32;
+//typedef unsigned int uint32;
+//typedef long long int64;
+//typedef unsigned long long uint64;
+//typedef unsigned char ubyte;
+//
+//#define MAX(a, b) a < b ? b : a
+//#define MIN(a, b) a < b ? a : b
+//#define ABS(a) a < 0 ? -a : a
+//
+//typedef int64 INT;
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	INT n;
+//	cin >> n;
+//
+//	INT ret = 0;
+//
+//	for (INT i = 3; i <= n; i += 3) {
+//		for (INT j = 3; j <= n; j += 3) {
+//			if (i + j < n) {
+//				ret++;
+//			}
+//		}
+//	}
+//
+//	cout << ret;
+//
+//	return 0;
+//}
--- cmm/main.cpp
+++ cmm/main.cpp
@@ -20,63 +20,25 @@ typedef unsigned char ubyte;
 
 typedef int64 INT;
 
-constexpr INT MAX_N = 10;
-constexpr INT MAX_M = 10;
-constexpr INT MAX_VALUE = 10000;
-
-INT N, M;
-INT allSolved;
-INT students[MAX_M];
-
-INT ret = MAX_VALUE;
-
-void calculate(INT count, INT current, INT solved) {
-	if (current == M) {
-		if (solved == allSolved) {
-			ret = MIN(ret, count);
-		}
-
-		return;
-	}
-
-	calculate(count, current + 1, solved);
-	calculate(count + 1, current + 1, solved | students[current]);
-}
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N >> M;
-	allSolved = 0;
+	INT n;
+	cin >> n;
 
-	for (INT n = 0; n < N; n++) {
-		allSolved |= (1ll << n);
-	}
+	INT ret = 0;
 
-	for (INT m = 0; m < M; m++) {
-		INT t;
-		cin >> t;
-
-		students[m] = 0;
-
-		for (INT i = 0; i < t; i++) {
-			INT problem;
-			cin >> problem;
-
-			students[m] |= (1ll << (problem - 1));
+	for (INT i = 3; i <= n; i += 3) {
+		for (INT j = 3; j <= n; j += 3) {
+			if (i + j < n) {
+				ret++;
+			}
 		}
 	}
 
-	calculate(0ll, 0ll, 0ll);
-
-	if (ret == MAX_VALUE) {
-		cout << -1;
-	}
-	else {
-		cout << ret;
-	}
+	cout << ret;
 
 	return 0;
 }
